2ec7c869679f488e6dea5a45d72e2898c7ef1c1c
nilima kafle <nilimakafle2018@gmail.com>
2025-11-05 07:46:19 -0600

    completed navbar for landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,49 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function Home() {
   return (
-    <>
-      <h1>Mini Online Course for Process Feedback...</h1>
-    </>
-  )
+    
+    <div className="home-container">
+
+      <h1>Welcome to the mini online course of Process Feedback</h1>
+
+      <p>
+        This mini online course is designed to help Process Feedback users become familiar with our application. 
+        The introductory video will guide you step-by-step on how to navigate the Process Feedback tool, 
+        which you will use to complete all your assignments in this course. Please make sure you understand 
+        the material thoroughly so your semester can run smoothly without interruptions. If you have any 
+        questions or concerns, feel free to reach out to us by clicking Contact Us in the navigation bar 
+        and submitting the form. If you would like to learn more about Process Feedback, simply click About 
+        Process Feedback in the navigation bar.
+      </p>
+    </div>
+  );
 }
 
-export default App
+function About() {
+  return <h1>About Process Feedback</h1>;
+}
+
+function Contact() {
+  return <h1>Contact Us</h1>;
+}
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Header.css";
+import logo from "../../assets/PFLogo.png";
+
+function Header() {
+  return (
+    <header className="header">
+      {/*PF logo on the left side in navbar*/}
+      <div className="header-left">
+        <img src={logo} alt="Process Feedback Logo" className="logo" />
+      </div>
+
+      {/*Navigaion links on the right side in navbar*/}
+      <nav className="header-right">
+        <a 
+            href="https://www.processfeedback.org/about/"
+            target="_blank" //open in the new tab so my my current app opens still 
+        >
+            About Process Feedback 
+        </a>
+        <a href="/contact">Contact Us</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
